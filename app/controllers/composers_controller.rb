8b539838a5a1ac3c0b1f178778f9b5c364958ed3
Rob Head <robert.head@gmail.com>
2016-11-27 21:44:49 -0800

    Add latest year to dataset.

--- app/controllers/composers_controller.rb
+++ app/controllers/composers_controller.rb
@@ -1,7 +1,9 @@
 class ComposersController < ApplicationController
   def index
     composers = Composer.order(:birth_year).where.not(birth_year: nil).where.not(death_year: nil)
-    earliest_year = composers.pluck(:birth_year).min
-    render json: { composers: composers, earliest_year: earliest_year }
+    birth_years = composers.pluck(:birth_year)
+    earliest_birth_year = birth_years.min
+    latest_birth_year = birth_years.max
+    render json: { composers: composers, earliest_birth_year: earliest_birth_year, latest_birth_year: latest_birth_year }
   end
 end
