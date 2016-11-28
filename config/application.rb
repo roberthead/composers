47dde1dfff51e7a6d6d651ed15a0e86cf0ae9425
Rob Head <robert.head@gmail.com>
2016-11-27 18:03:35 -0800

    Attempt to whitelist codepen.io.

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,18 @@ Bundler.require(*Rails.groups)
 
 module Composers
   class Application < Rails::Application
-    
-  config.generators do |g|
-    g.orm             :active_record
-    g.template_engine :haml
-    g.test_framework  :rspec, fixture: false
-    g.stylesheets     false
-    g.javascripts     false
-  end
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :haml
+      g.test_framework  :rspec, fixture: false
+      g.stylesheets     false
+      g.javascripts     false
+    end
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://*.codepen.io',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
