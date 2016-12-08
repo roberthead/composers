9e41bfb85160515022ad5009f4e515ba0d178208
Rob Head <robert.head@gmail.com>
2016-12-08 10:30:48 -0800

    Add era spec.

--- spec/models/era_spec.rb
+++ spec/models/era_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Era, type: :model do
   describe '.from_string' do
+    specify { expect(Era.from_string('List_of_medieval_composers').name).to eq "Medieval" }
     specify { expect(Era.from_string('List_of_Renaissance_composers').name).to eq "Renaissance" }
     specify { expect(Era.from_string('List_of_20th-century_classical_composers').name).to eq "Modernist" }
   end
