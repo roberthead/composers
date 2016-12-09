23852c1b2441e2471b261f5d5b2c0a46148eaf7a
Rob Head <robert.head@gmail.com>
2016-12-09 05:45:49 -0800

    Ignore composers before 1100.

--- app/controllers/composers_controller.rb
+++ app/controllers/composers_controller.rb
@@ -1,6 +1,9 @@
 class ComposersController < ApplicationController
   def index
-    composers = Composer.where.not(birth_year: nil).where.not(death_year: nil).order(importance: :desc).limit(200)
+    composers = Composer.where.not(birth_year: nil).
+      where.not(death_year: nil).
+      where("birth_year > ?", 1100).
+      order(importance: :desc).limit(200)
     birth_years = composers.pluck(:birth_year)
     death_years = composers.pluck(:death_year)
     render json: {
