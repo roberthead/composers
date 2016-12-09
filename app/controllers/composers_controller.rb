89e78ed9747a2c4a840051e7e6efa7f6c0f0d5f1
Rob Head <rob@ministryofvelocity.com>
2016-12-08 22:06:50 -0800

    Fix sort_by argument.

--- app/controllers/composers_controller.rb
+++ app/controllers/composers_controller.rb
@@ -4,7 +4,7 @@ class ComposersController < ApplicationController
     birth_years = composers.pluck(:birth_year)
     death_years = composers.pluck(:death_year)
     render json: {
-      composers: composers.sort_by(:birth_year),
+      composers: composers.sort_by(&:birth_year),
       earliest_birth_year: birth_years.min,
       latest_birth_year: birth_years.max,
       earliest_death_year: death_years.min,
