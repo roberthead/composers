41f9732bc19f7cce997859e2354932511a854e8d
Rob Head <rob@ministryofvelocity.com>
2016-12-09 02:33:38 -0800

    draft of graph.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 
 let url = 'http://composers-api.herokuapp.com/composers.json'
 let earliestBirthYear, latestDeathYear, yearsTotal
+let stageWidth = 1600
 
 let eraColor = {
   renaissance: "#339933",
@@ -29,62 +30,69 @@ let eraColor = {
 
 //This function contains the logic for rendering our image from a dataset
 let updateGraph = function(dataset) {
-	//select a specific set of objects inside of the dataset
+  //select a specific set of objects inside of the dataset
   let svgContainer = d3.select('svg')
-	let timelines = svgContainer.selectAll('line').data(dataset)
-	let texts = svgContainer.selectAll('text').data(dataset)
+
+  let lifelines = svgContainer.selectAll('line').data(dataset)
+  let texts = svgContainer.selectAll('text').data(dataset)
 
   let currentY = 0
-	let age, ellapsedPortion = 0
-	let red, green, blue = 0
+  let age, ellapsedPortion = 0
+  let red, green, blue = 0
   let lifespan
 
-  timelines.enter().append('line')
-		.attr('x1', function(composer) {
+  lifelines.enter().append('line')
+    .attr('x1', function(composer) {
       return (composer.birth_year - earliestBirthYear) * 2 + 25
-		})
-		.attr('y1', function (composer, index) {
+    })
+    .attr('y1', function (composer, index) {
       return (index * 25) % 800 + 47
-		})
-		.attr('x2', function(composer) {
-			lifespan = composer.death_year - composer.birth_year;
+    })
+    .attr('x2', function(composer) {
+      lifespan = composer.death_year - composer.birth_year;
       return (composer.birth_year - earliestBirthYear) * 2 + 25 + lifespan * 2
-		})
+    })
     .attr('y2', function(composer, index) {
       return (index * 25) % 800 + 47
     })
-		.attr('stroke', function (composer) {
+    .attr('stroke', function (composer) {
       return eraColor[composer.primary_era.toLowerCase()]
-		})
-		.attr('stroke-width', function (composer) {
-			return 2;
-		})
-		.style('opacity', 1)
+    })
+    .attr('stroke-width', function (composer) {
+      return 2
+    })
+    .style('opacity', 1)
 
-    texts.enter().append('text')
-      .attr('x', function(composer) {
-        return (composer.birth_year - earliestBirthYear) * 2 + 25
-      })
-      .attr('y', function(composer, index) {
-        return (index * 25) % 800 + 45
-      })
-      .text(function(composer) { return composer.name })
-      .attr("font-family", "Open Sans, sans-serif")
-      .attr("font-size", function (composer) {
-        return Math.pow(composer.google_results_count + composer.wikipedia_page_length, 0.23)
-      })
+  texts.enter().append('text')
+    .attr('x', function(composer) {
+      return (composer.birth_year - earliestBirthYear) * 2 + 25
+    })
+    .attr('y', function(composer, index) {
+      return (index * 25) % 800 + 45
+    })
+    .text(function(composer) { return composer.name })
+    .attr("font-family", "Open Sans, sans-serif")
+    .attr("font-size", function (composer) {
+      return Math.pow(composer.google_results_count + composer.wikipedia_page_length, 0.23)
+    })
 }
 
 // this function contains the logic for changing the data
 var parseData = function(dataset) {
-	//use any logic to parse/update your data here
-	composers = dataset.composers
-	earliestBirthYear = dataset.earliest_birth_year
-	latestDeathYear = dataset.latest_death_year
-	yearsTotal = latestDeathYear - earliestBirthYear
+  //use any logic to parse/update your data here
+  composers = dataset.composers
+  earliestBirthYear = dataset.earliest_birth_year
+  latestDeathYear = dataset.latest_death_year
+  yearsTotal = latestDeathYear - earliestBirthYear
 
-	//now render the visual
-	updateGraph(composers);
+  // render timeline
+  let pixelsPerYear = stageWidth.to_f / yearsTotal
+  let svgContainer = d3.select('svg')
+  let centuryMarkData = [1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000]
+  let centuryMarks = svgContainer.selectAll('line').data(centuryMarkData)
+
+  //now render the visual
+  updateGraph(composers);
 }
 
 d3.json(url, parseData);
